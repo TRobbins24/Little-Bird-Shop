b070110249a08c1794be985bab83f2d05aa0450f
TRobbins24 <84456198+TRobbins24@users.noreply.github.com>
2022-04-29 10:23:50 -0400

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+let numContainer = document.getElementById("cartNumber");
+let value = 0;
+let btnInc = document.querySelector("#buttonCart");
+btnInc.addEventListener("click", () => {
+  value++;
+  numContainer.textContent = value;
+});
+
