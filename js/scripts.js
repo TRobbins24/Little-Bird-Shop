8040c614612e928f3c2b5de0877f679bf2e69145
TRobbins24 <84456198+TRobbins24@users.noreply.github.com>
2022-04-29 10:22:44 -0400

    Delete js directory

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-let numContainer = document.getElementById("cartNumber");
-let value = 0;
-let btnInc = document.querySelector("#buttonCart");
-btnInc.addEventListener("click", () => {
-  value++;
-  numContainer.textContent = value;
-});
